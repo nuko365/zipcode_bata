30733c466d3e5fea8dc2425dd472fa59cc09e3f8
nuko365 <nuko365@gmail.com>
2018-07-03 17:18:22 +0900

    ・整形

---
+++ htdocs/api/postal_codes/search.php
@@ -0,0 +1,31 @@
+<?php
+$dir = __DIR__ . '/data';
+$result = array();
+
+// GET Parameter Check
+if (!isset($_GET['code'])) {
+    //http_response_code(400);
+    $result = array('error' => 'Get Parameter is Invalid', 'http_code' => '400');
+} elseif(!preg_match("/^\d{7}$/", $_GET['code'])) {
+    //http_response_code(400);
+    $result = array('error' => 'Postal Code is Invalid', 'http_code' => '400');
+} else {
+    // Address Search
+    $result = array();
+    $code = $_GET['code'];
+    $file = $dir . DIRECTORY_SEPARATOR . substr($code, 0, 1) . '.csv';
+    if(file_exists($file)) {
+        $spl = new SplFileObject($file);
+        while (!$spl->eof()) {
+            $columns = $spl->fgetcsv();
+            if(isset($columns[0]) && $columns[0] === $code){
+                $result = array('prefecture' => $columns[1], 'city' => $columns[2], 'address_line' => $columns[3]);
+                break;
+            }
+        }
+    }
+}
+
+// Response
+header('Content-type: application/javascript; charset=utf-8');
+echo json_encode($result);
