db9ac39d96105f201c9fa9f0ab4c56ff77a15d1b
nuko365 <nuko365@gmail.com>
2018-07-03 15:26:01 +0900

    GETパラメータに変更

--- htdocs/api/postal_codes/api.php
+++ htdocs/api/postal_codes/api.php
@@ -1,33 +1,42 @@
 <?php
 $dir = __DIR__ . '/zipcode';
- 
+
+$zipcode = $_GET['zipcode'];
+//$array_data = array('zipcode' => $zipcode);
+//header('Content-type: application/javascript; charset=utf-8');
+//echo json_encode($data);
+//exit;
+
 // Ajax以外からのアクセスを遮断
-$request = (string)filter_input(INPUT_SERVER, 'HTTP_X_REQUESTED_WITH');
-if(strtolower($request) !== 'xmlhttprequest') exit;
+//$request = (string)filter_input(INPUT_SERVER, 'HTTP_X_REQUESTED_WITH');
+//if(strtolower($request) !== 'xmlhttprequest') exit;
  
-$json = file_get_contents('php://input');
-$data = json_decode($json, true);
-file_put_contents('test.log', print_r($data, true));
-$zipcode = !empty($data) ? $data : '';
-$zipcode = mb_convert_kana($zipcode, 'a', 'utf-8');
-$zipcode = preg_replace('/[\sー-]/', '', $zipcode);
+//$json = file_get_contents('php://input');
+
+//$data = json_decode($json, true);
+//file_put_contents('test.log', print_r($data, true));
+//$zipcode = !empty($data) ? $data : '';
+//$zipcode = mb_convert_kana($zipcode, 'a', 'utf-8');
+//$zipcode = preg_replace('/[\sー-]/', '', $zipcode);
  
-$callback  = (string)filter_input(INPUT_GET, 'callback');
-$callback  = htmlspecialchars(strip_tags($callback));
+//$zipcode  = (string)filter_input(INPUT_GET, 'callback');
+//$callback  = htmlspecialchars(strip_tags($callback));
  
-$param = array('', '', '');
- 
+$result = array();
+
+// Search
 $file = $dir . DIRECTORY_SEPARATOR . substr($zipcode, 0, 1) . '.csv';
-if(file_exists($file)){
+if(file_exists($file)) {
     $spl = new SplFileObject($file);
     while (!$spl->eof()) {
         $columns = $spl->fgetcsv();
         if(isset($columns[0]) && $columns[0] == $zipcode){
-            $param = array($columns[1], $columns[2], $columns[3]);
+            // TODO: $result = array($columns[1], $columns[2], $columns[3]);
+            $result = array('prefecture' => $columns[1], 'city' => $columns[2], 'address_line' => $columns[3]);
             break;
         }
     }
 }
- 
+
 header('Content-type: application/javascript; charset=utf-8');
-printf("{$callback}(%s)", json_encode( $param ));
+echo json_encode($result);
